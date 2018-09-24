2ddf6292932ac98e97c3f7f0273dfb2841c0d62a
David Mills <dave@1morepx.com>
2018-09-24 16:07:31 -0400

    summing the two number provided
    also handling converting to numbers from strings

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var args = process.argv;
 
-var number1 = args[2];
-var number2 = args[3];
+var number1 = parseInt(args[2], 10);
+var number2 = parseInt(args[3], 10);
 
 console.log("Number 1:", number1);
 console.log("Number 2:", number2);
+
+var sum = number1 + number2;
+console.log("Result:", sum);
